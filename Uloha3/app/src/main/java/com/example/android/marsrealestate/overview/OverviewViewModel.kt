672dc4f0f39800d9375ea48c3bb42b5ed8c9d82e
BackoKillerSVK <MBBaco@gmail.com>
2022-12-13 17:42:39 +0100

    Commit

---
+++ Uloha3/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.android.marsrealestate.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.marsrealestate.network.MarsApi
+import kotlinx.coroutines.launch
+
+
+
+class OverviewViewModel : ViewModel() {
+
+    // The internal MutableLiveData String that stores the most recent response
+    private val _response = MutableLiveData<String>()
+
+    // The external immutable LiveData for the response String
+    val response: LiveData<String>
+        get() = _response
+
+
+    init {
+        getMarsRealEstateProperties()
+    }
+
+
+    private fun getMarsRealEstateProperties() {
+        viewModelScope.launch {
+            try {
+                val listResult = MarsApi.retrofitService.getProperties()
+                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+            } catch (e: Exception) {
+                _response.value = "Failure: ${e.message}"
+            }
+        }
+    }
+}
